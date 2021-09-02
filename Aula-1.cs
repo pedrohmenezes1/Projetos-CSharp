413ab00ab5f2418f69494d475ca580e69b631dca
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2021-09-01 21:32:50 -0300

    Aulas C# Uninassau

---
+++ Aula-1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aulas_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
