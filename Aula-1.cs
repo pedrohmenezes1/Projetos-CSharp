4c22a78589bf1daffe9cdea90a2ff5d7976cd400
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2021-09-01 21:43:46 -0300

    Aula 1 Uninassau

--- Aula-1.cs
+++ Aula-1.cs
@@ -7,6 +7,7 @@ namespace Aulas_CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia");
         }
     }
 }
